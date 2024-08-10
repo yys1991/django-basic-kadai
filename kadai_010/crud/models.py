78550202138e5f8250cbccaddec6fd55c3b41215
yys1991 <yu.shimizu1991@gmail.com>
2024-08-10 10:58:28 +0900

    Update models.py

--- kadai_010/crud/models.py
+++ kadai_010/crud/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
