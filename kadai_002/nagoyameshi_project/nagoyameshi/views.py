dc58de19f3bbe77b37d5bde9fd890e6e266db21d
yys1991 <yu.shimizu1991@gmail.com>
2024-08-30 10:41:12 +0900

    Add files via upload

--- kadai_002/nagoyameshi_project/nagoyameshi/views.py
+++ kadai_002/nagoyameshi_project/nagoyameshi/views.py
@@ -5,17 +5,42 @@ class TopView(TemplateView):
     template_name="top.html"
 
 from django.views import View
-from.models import Restaurant
+from.models import Restaurant,Category
+
+from django.db.models import Q
+
 
 class TopView(View):
     def get(self,request):
+        query = Q()
+
         if"search" in request.GET:
-            print(request.get["search"])
+           print(request.GET["search"])
 
-        restaurants=Restaurant.objects.all()
 
-        context={"restaurants":restaurants,
-                 "greet":"こんにちは",
-                 "price":1000
-        }         
+           words = request.GET["search"].replace("　"," ").split(" ")
+           print(words)
+
+
+           for word in words:
+               query &= Q(name__icontains=word)
+
+
+        if "category" in request.GET:
+            print( request.GET["category"] )       
+            if "" != request.GET["category"]:
+                query &= Q(category=request.GET["category"])
+
+
+        restaurants = Restaurant.objects.filter(query)
+
+        categories = Category.objects.all()
+
+        context={
+            "restaurants":restaurants,
+            "greet":"こんにちは",
+            "price":1000,
+            "categories":categories
+        }
+
         return render(request,"top.html",context)
